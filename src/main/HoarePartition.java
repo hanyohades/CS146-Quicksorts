ccf8e022999c8d455425819a9bffe10bee80c87f
hanyohades <hanyohades0@gmail.com>
2026-02-10 13:43:10 -0800

    add comments

--- src/main/HoarePartition.java
+++ src/main/HoarePartition.java
@@ -1,14 +1,29 @@
+/*
+Hoare Parition method is taking the first element in the array as pivot.
+Using two pointers i start at low-1 and j start at high+1 of the array.
+if the element at i is less than pivot we will keep increasing i
+if the element at j is greate than pivot we will keep decreasing j
+if i greater and equal to j we return j
+None of those if conditions are true, we will swap element at i with element at j
+Because that will create 
+all the element half left smaller than pivot and half right greater than pivot.
+*/
+
 public class HoarePartition {
     public static int partition(int[] arr, int low, int high) {
         int pivot = arr[low];
         int i = low - 1;
         int j = high + 1;
         
+        // Loop will run until i >= j and return index j
         while (true) {
+
+            // increment i by 1 first then check if true do again
             do {
                 i++;
             } while (arr[i] < pivot);
 
+            //decrement j by 1 first then check if true do again.
             do {
                 j--;
             } while (arr[j] > pivot);
@@ -16,7 +31,8 @@ public class HoarePartition {
             if (i >= j) {
                 return j;
             }
-
+            
+            //if arr[i] is not less pivot and arr[j] is not greater pivot we swap.
             int temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
@@ -27,7 +43,10 @@ public class HoarePartition {
         if (low < high) {
             int pivotIndex = partition(arr, low, high);
             
+            //recursion for half left part
             quickSort(arr, low, pivotIndex);
+
+            //recursion for half right part
             quickSort(arr, pivotIndex + 1, high);
         }
     }
--- src/main/LomutoPartition.java
+++ src/main/LomutoPartition.java
@@ -1,12 +1,24 @@
+/*
+Lomuto Partition method is taking the last element in the array as pivot
+loop j through all the elements in array
+check if the element at j is less than or equal to pivot
+if yes we increment i by 1 and do the swap with the element at j index.
+final step swap the element at i+1 with pivot and return i+1 (next index of i).
+*/
 public class LomutoPartition {
 
     public static int partition(int[] arr, int low, int high) {
+        //get the last element in arr
         int pivot = arr[high];
 
         int i = low - 1;
 
+        //j start from the left (low) and end at the last element
         for (int j = low; j < high; j++) {
-            if (arr[j] <= pivot) {
+
+            /*if element at j index is less than pivot
+             we increment i by 1 and do the swap */
+            if (arr[j] < pivot) {
                 i++;
 
                 int temp = arr[i];
@@ -15,6 +27,7 @@ public class LomutoPartition {
             }
         }
 
+        //swap index at i+1 with the pivot
         int temp = arr[i + 1];
         arr[i + 1] = arr[high];
         arr[high] = temp;
@@ -23,9 +36,11 @@ public class LomutoPartition {
     }
 
     public static void quickSort(int[] arr, int low, int high) {
+        //only execute if the begin and the end of the index is not cross or equal 
         if (low < high) {
             int pivotIndex = partition(arr, low, high);
 
+            // Recurse on the two sides excluding the pivot index (pi),
             quickSort(arr, low, pivotIndex - 1);
             quickSort(arr, pivotIndex + 1, high);
         }
